8f47065c2c7f0b05d5f37117e31888229a2969df
TakeshiAono <takeshi.aono001@gmail.com>
2024-12-03 01:03:46 +0900

    ダブルクリック後、エラーになるため修正

--- components/solitair/DumpCardArea.tsx
+++ components/solitair/DumpCardArea.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleProp, View } from "react-native";
+import { StyleProp, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
 import { ViewStyle } from "react-native";
@@ -10,7 +10,7 @@ import TrumpCard from "../TrumpCard";
 
 type Props = {
   mark: Exclude<TrampCard["type"], "joker">;
-  number?: number;
+  number?: number | null;
   scale?: number;
   isHide?: boolean;
   style?: StyleProp<ViewStyle>;
@@ -19,8 +19,27 @@ type Props = {
 const DumpCardArea = observer(({ number, mark }: Props) => {
   return (
     <View>
-      {number ? (
-        <TrumpCard number={number} mark={mark} scale={70} />
+      {!!number ? (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 100,
+            width: 50,
+            borderWidth: 1,
+            // borderStyle: "dashed",
+            borderRadius: 10,
+            backgroundColor: "white",
+          }}
+        >
+          <Text> {number}</Text>
+          {mark === "cards-club" || mark === "cards-spade" ? (
+            <Icon2 name={mark} size={20} color={"black"} />
+          ) : (
+            <Icon name={mark} size={20} color={MarkColorMap[mark]} />
+          )}
+        </View>
       ) : (
         <View
           style={{
